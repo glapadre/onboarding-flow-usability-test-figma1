2034737f610992f7ed0cf528701f09ff489eddf2
Drew-Glapa <Drew.Glapa@ibm.com>
2022-09-06 15:48:04 -0400

    first commit

---
+++ data/story.js
@@ -0,0 +1,268 @@
+var story = {
+ "docName": "Onboarding Flow Usability Test Prototype",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableInteractions": false,
+ "disableHotspots": true,
+ "hideGallery": true,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "575:186177",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "ProductDetailPage",
+   "image": "productdetailpage.png",
+   "image2x": "productdetailpage@2x.png",
+   "index": 0,
+   "width": 1312,
+   "height": 2734,
+   "x": -16586,
+   "y": 9970,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 848,
+      "y": 92,
+      "width": 196,
+      "height": 30
+     },
+     "index": 0,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "937:186172",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "SignIn_AWS",
+   "image": "signin-aws.png",
+   "image2x": "signin-aws@2x.png",
+   "index": 1,
+   "width": 1312,
+   "height": 833,
+   "x": -15096,
+   "y": 9958,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 256,
+      "y": 351,
+      "width": 281,
+      "height": 46
+     },
+     "index": 1,
+     "page": 2
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "936:186067",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "AWSContract1",
+   "image": "awscontract1.png",
+   "image2x": "awscontract1@2x.png",
+   "index": 2,
+   "width": 1312,
+   "height": 1505,
+   "x": -13606,
+   "y": 9966,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 336,
+      "y": 285,
+      "width": 642,
+      "height": 60
+     },
+     "index": 2,
+     "page": 3
+    },
+    {
+     "rect": {
+      "x": 337,
+      "y": 421,
+      "width": 359,
+      "height": 57
+     },
+     "index": 3,
+     "page": 4
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "936:186093",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "AWSContract1(1)",
+   "image": "awscontract1(1).png",
+   "image2x": "awscontract1(1)@2x.png",
+   "index": 3,
+   "width": 1312,
+   "height": 1505,
+   "x": -12174,
+   "y": 9958,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 336,
+      "y": 290,
+      "width": 639,
+      "height": 143
+     },
+     "index": 4,
+     "page": 2
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "936:186100",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "AWSContract1(2)",
+   "image": "awscontract1(2).png",
+   "image2x": "awscontract1(2)@2x.png",
+   "index": 4,
+   "width": 1312,
+   "height": 1505,
+   "x": -10774,
+   "y": 9958,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 737,
+      "y": 509,
+      "width": 119,
+      "height": 38
+     },
+     "index": 5,
+     "page": 5
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "936:186120",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "AWSContract1(3)",
+   "image": "awscontract1(3).png",
+   "image2x": "awscontract1(3)@2x.png",
+   "index": 5,
+   "width": 1312,
+   "height": 833,
+   "x": -9239,
+   "y": 9970,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 785,
+      "y": 544,
+      "width": 159,
+      "height": 45
+     },
+     "index": 6,
+     "page": 6
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "937:186134",
+   "groupID": "393:200511",
+   "transAnimType": 0,
+   "title": "AWSContract1(4)",
+   "image": "awscontract1(4).png",
+   "image2x": "awscontract1(4)@2x.png",
+   "index": 6,
+   "width": 1312,
+   "height": 833,
+   "x": -7748,
+   "y": 9970,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 556,
+      "y": 530,
+      "width": 213,
+      "height": 57
+     },
+     "index": 7,
+     "url": "https://pages.github.ibm.com/Drew-Glapa/onboarding-flow-usability-test/"
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "188:353540",
+   "name": "Cover"
+  },
+  {
+   "id": "199:354548",
+   "name": "About"
+  },
+  {
+   "id": "188:353566",
+   "name": "------Latest-----------------"
+  },
+  {
+   "id": "163:351403",
+   "name": "Discover, Try, Buy on AWS"
+  },
+  {
+   "id": "393:200511",
+   "name": "Prototype for usability testing"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 7,
+ "backColor": "#FFFFFF"
+}
